b125fb9c71c704551717c429f7871ec848f3f898
ankit <iankitkhatrani@gmail.com>
2024-05-30 17:54:53 +0530

    add

--- helper/poker/cardDeal.js
+++ helper/poker/cardDeal.js
@@ -42,6 +42,8 @@ module.exports.cardDealStart = async (tbid) => {
         dealerSeatIndex: tabInfo.dealerSeatIndex,
         smallblindSeatIndex: tabInfo.smallblindSeatIndex,
         bigblindSeatIndex: tabInfo.bigblindSeatIndex,
+        bigblind: bigblind,
+        smallblind:smallblind
     }
     commandAcions.sendEventInTable(tabInfo._id.toString(), CONST.TABLE_CARD_DEAL, eventResponse);
 
